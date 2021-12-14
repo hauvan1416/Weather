467e45c7139476a67b57739564765f19c672e4cb
Hậu Văn <hauvan1416@gmail.com>
2021-12-14 19:43:27 +0700

    update

--- js/index.js
+++ js/index.js
@@ -20,6 +20,7 @@ searchInput.addEventListener('change', (e) => {
         console.log('[Search Input]', data);
         cityName.innerHTML = data.name || DEFAULT_VALUE;
         weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+        weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
         temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
         sunrise.innerHTML = moment.unix(data.sys.sunrise).format('H:mm') || DEFAULT_VALUE;
         sunset.innerHTML = moment.unix(data.sys.sunset).format('H:mm') || DEFAULT_VALUE;
